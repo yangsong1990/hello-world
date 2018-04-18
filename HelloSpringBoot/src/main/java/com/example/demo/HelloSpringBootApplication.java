f4f8318a9409b823fa93374e6dfdca52f450ba8d
yangsong1990 <yangsong@192.168.1.106>
2018-04-19 02:32:22 +0800

    整合redis

--- HelloSpringBoot/src/main/java/com/example/demo/HelloSpringBootApplication.java
+++ HelloSpringBoot/src/main/java/com/example/demo/HelloSpringBootApplication.java
@@ -9,4 +9,5 @@ public class HelloSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
+
 }
--- HelloSpringBoot/src/main/java/com/example/demo/controller/HelloController.java
+++ HelloSpringBoot/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,20 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private StringRedisTemplate srt;
+	
 	@RequestMapping("/hello")
 	public Object sayHello() {
+		//String user =  srt.opsForValue().get("user");
+		//System.out.println(user);
+		srt.opsForValue().set("json:name", "{name:'yangsong'}");
 		return "Hello Spring-Boot!";
 	}
-	
-	@RequestMapping("/user")
-	public Object getUser() {
-		return "Hello Spring-Boot!";
-	}
-
 
 }
